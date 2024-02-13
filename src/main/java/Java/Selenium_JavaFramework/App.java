3a27be58a7a78b0ef8cdf54702dd2d934f877abd
Mukul Sharma <mukulsharma718@gmail.com>
2024-02-13 13:00:58 +0530

    Setting up the maven project

---
+++ src/main/java/Java/Selenium_JavaFramework/App.java
@@ -0,0 +1,7 @@
+package Java.Selenium_JavaFramework;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/Java/Selenium_JavaFramework/AppTest.java
@@ -0,0 +1,13 @@
+package Java.Selenium_JavaFramework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
